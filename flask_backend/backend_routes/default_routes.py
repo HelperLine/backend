3f9502f5d8687fb6398819cb0ec7d6d7fb5c0cfc
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-04-15 23:39:20 +0200

    Add swagger documentation to a static url #36

--- flask_backend/backend_routes/default_routes.py
+++ flask_backend/backend_routes/default_routes.py
@@ -1,5 +1,7 @@
 
 from flask_backend import app
+from flask_backend.support_functions import formatting
+from flask import redirect
 
 
 @app.route("/", methods=["GET"])
@@ -7,3 +9,12 @@ def route_index():
     return "<p>This is the helperline backend. See <a href=" \
            "'https://helperline.io/'>helperline.io</a> for more.</p>"
 
+
+@app.route("/<api_version>/docs")
+def route_docs(api_version):
+
+    if api_version == "v1":
+        return redirect("https://app.swaggerhub.com/apis-docs/HelperLine/BackendAPI/1.0")
+
+    else:
+        return formatting.status("api_version invalid")
